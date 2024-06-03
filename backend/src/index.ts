7cc61bc8349e890b3c1847deaf7c279f9625d077
Malarvizhi <tamilmalar181103@gmail.com>
2024-06-03 07:10:45 +0530

    setup mern

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express,{Request,Response} from 'express';
+import cors from 'cors';
+import "dotenv/config";
+import mongoose from 'mongoose';
+mongoose.connect(process.env.mongo_db_connection_string as string);
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
+
+app.get("/api/test",async(req: Request, res: Response)=>{
+ res.json({message: "hello from express endpoint!"});
+});
+app.listen(7000, ()=>{
+    console.log("server is running in localhost: 7000");
+})
